9b19302c2ce0aba1fd1aa64342cb851f6f82143d
KRIN PATEL <162860250+KRIN-PATEL@users.noreply.github.com>
2025-04-16 00:32:47 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+ server: {
+    host: "0.0.0.0",                // Required for Render
+    port: process.env.PORT || 5173  // Render uses dynamic port
+  },
 })
