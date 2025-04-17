3113a176aa4663ca4c87e720ab7d1ac680ec0d3d
KRIN PATEL <162860250+KRIN-PATEL@users.noreply.github.com>
2025-04-16 23:41:48 -0400

    Update SearchPage.jsx

--- src/pages/client/SearchPage.jsx
+++ src/pages/client/SearchPage.jsx
@@ -32,7 +32,7 @@ const SearchPage = () => {
   };
 
   return (
-    <div className="max-w-7xl mx-auto p-4 md:p-8">
+    <div className="max-w-7xl mt-20 mx-auto p-4 md:p-8">
       <div className="my-6">
         <h1 className="font-bold text-xl md:text-2xl">Result for "{query}"</h1>
         <p>
