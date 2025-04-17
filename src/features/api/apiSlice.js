30120f28cea45aad2491323d063518747cf9fb7e
KRIN PATEL <162860250+KRIN-PATEL@users.noreply.github.com>
2025-04-17 00:18:22 -0400

    Update apiSlice.js

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,9 +1,12 @@
+
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-    reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_API_URL}/api/v1` }),
-     credentials: "include",
-    tagTypes: ["Quiz", "Course", "User"],
-    endpoints: () => ({}),
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_API_URL}/api/v1`,
+    credentials: "include", 
+  }),
+  tagTypes: ["Quiz", "Course", "User"],
+  endpoints: () => ({}),
 });
