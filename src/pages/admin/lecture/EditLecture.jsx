03ad57d6f01e549d7ece4fc630577b8030523fce
KRIN PATEL <162860250+KRIN-PATEL@users.noreply.github.com>
2025-04-17 00:51:58 -0400

    Update EditLecture.jsx

--- src/pages/admin/lecture/EditLecture.jsx
+++ src/pages/admin/lecture/EditLecture.jsx
@@ -10,7 +10,7 @@ const EditLecture = () => {
   const courseId = params.courseId;
   return (
     <div>
-      <div className="flex items-center justify-between mb-5 mt-10">
+      <div className="flex items-center justify-between mb-5 mt-20">
         <div className="flex items-center gap-2">
           <Link to={`/admin/course/${courseId}/lecture`}>
             <Button size="icon" variant="outline" className="rounded-full">
